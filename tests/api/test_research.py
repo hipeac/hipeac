a7783c8283d35187f5676e505801d1d0ee0ea083
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2018-10-04 16:12:12 +0200

    tests(api): required query parameter

--- tests/api/test_research.py
+++ tests/api/test_research.py
@@ -8,7 +8,8 @@ class TestForAnonymous:
     list_url = reverse('v1:paper-award-list')
 
     def test_list(self, api_client, db):
-        assert api_client.get(self.list_url).status_code == status.HTTP_200_OK
+        assert api_client.get(self.list_url).status_code == status.HTTP_403_FORBIDDEN
+        assert api_client.get(self.list_url + '?year=2018').status_code == status.HTTP_200_OK
 
     def test_create(self, api_client):
         assert api_client.post(self.list_url).status_code == status.HTTP_405_METHOD_NOT_ALLOWED
