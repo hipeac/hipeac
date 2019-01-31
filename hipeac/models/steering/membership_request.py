9c87ef1591dcef9b5fe33e988e2bf1cbb31423ca
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2019-01-31 12:34:09 +0100

    feat: tweak membership emails with different message for associates

--- hipeac/models/steering/membership_request.py
+++ hipeac/models/steering/membership_request.py
@@ -7,11 +7,6 @@ class MembershipRequest(models.Model):
     """
     HiPEAC Membership request.
     """
-    STATUS_CHOICES = (
-        ('OK', 'Accepted'),
-        ('NO', 'Rejected'),
-        ('UN', 'Pending'),
-    )
     MEMBERSHIP_TYPE_CHOICES = (
         ('member', 'Member'),
         ('member,non-eu', 'Associated member'),
@@ -27,7 +22,7 @@ class MembershipRequest(models.Model):
     comments = models.TextField(null=True, blank=True)
     membership_type = models.CharField(db_index=True, max_length=16, null=True, blank=True,
                                        choices=MEMBERSHIP_TYPE_CHOICES)
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='UN')
+    accepted = models.BooleanField(default=None, null=True)
     decision_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -39,14 +34,15 @@ class MembershipRequest(models.Model):
         """
         Validates the model before saving.
         """
-        if self.status in ['OK', 'NO']:
+        if self.accepted is None:
+            self.decision_date = None
+        else:
             if self.decision_date is None:
                 raise ValidationError('A decision date is required for "Accepted" or "Rejected" membership requests.')
-        else:
-            self.decision_date = None
 
         if not self.email and not self.user:
             raise ValidationError('Please set a related User if no email is given.')
 
-    def clean_email(self):
+    @property
+    def clean_email(self) -> str:
         return self.user.email if self.user else self.email
--- hipeac/site/admin/steering.py
+++ hipeac/site/admin/steering.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.forms import ModelForm
+from django.urls import reverse
+from django.utils.html import format_html
 
 from hipeac.functions import send_task
 from hipeac.models import MembershipRequest
@@ -8,10 +10,10 @@ from hipeac.models import MembershipRequest
 def send_members_welcome(queryset):
     for instance in queryset:
         email = (
-            'users.members.welcome',
+            'users.members.welcome' if instance.membership_type == 'member' else 'users.members.non_eu.welcome',
             'Welcome to HiPEAC',
             'HiPEAC <membership@hipeac.net>',
-            ['eneko@illarra.com'],  # [instance.clean_email, 'membership@hipeac.net'],
+            [instance.clean_email, 'membership@hipeac.net'],
             {
                 'user_name': instance.name,
                 'is_registered': True if instance.user else False,
@@ -32,9 +34,10 @@ class MembershipRequestForm(ModelForm):
 
 @admin.register(MembershipRequest)
 class MembershipRequestAdmin(admin.ModelAdmin):
-    actions = ('make_accepted', 'make_rejected', 'send_members_welcome',)
-    list_display = ('id', 'name', 'affiliation', 'created_at', 'status')
-    list_filter = ('status',)
+    actions = ('make_rejected', 'send_members_welcome',)
+    date_hierarchy = 'created_at'
+    list_display = ('id', 'user_name', 'affiliation', 'accepted', 'decision_date')
+    list_filter = ('accepted',)
     search_fields = ('name', 'affiliation', 'email')
 
     raw_id_fields = ('user',)
@@ -46,20 +49,32 @@ class MembershipRequestAdmin(admin.ModelAdmin):
             'fields': ('website', 'motivation'),
         }),
         ('DECISION', {
-            'fields': ('status', 'decision_date', 'comments'),
+            'fields': ('accepted', 'decision_date', 'comments'),
         }),
     )
 
-    def make_accepted(self, request, queryset):
-        queryset.update(status='OK')
-    make_accepted.short_description = '[Bulk] Mark as accepted'
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('user__profile')
+
+    def user_name(self, obj):
+        if obj.user:
+            url = reverse('admin:auth_user_changelist')
+            return format_html(
+                f'<a href="{url}{obj.user_id}/" target="admin_user">{obj.user.profile.name}</a>'
+            )
+        return obj.name
 
     def make_rejected(self, request, queryset):
-        queryset.update(status='NO')
+        queryset.update(accepted=False)
     make_rejected.short_description = '[Bulk] Mark as rejected'
 
     def send_members_welcome(self, request, queryset):
-        queryset = queryset.filter(status='OK')
+        queryset = queryset.filter(accepted=True)
         send_members_welcome(queryset)
+        for membr in queryset.all():
+            if membr.user:
+                membr.user.profile.membership_tags = membr.membership_type
+                membr.user.profile.membership_date = membr.decision_date
+                membr.user.profile.save()
         admin.ModelAdmin.message_user(self, request, 'Emails are being sent.')
     send_members_welcome.short_description = ('[Mailer] Send welcome email (only if accepted)')
