8ef2f3f0d3360e5496e28d9c7cfb61df6c83261e
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2019-02-19 10:58:14 +0100

    chore(api): steering permissions

--- hipeac/api/permissions/__init__.py
+++ hipeac/api/permissions/__init__.py
@@ -2,3 +2,4 @@
 
 from .generic import *
 from .events import *
+from .steering import *
---
+++ hipeac/api/permissions/steering.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import BasePermission
+
+
+class SteeringPermission(BasePermission):
+
+    def is_steering(self, user=None):
+        if not hasattr(self, 'user_is_steering'):
+            self.user_is_steering = user.groups.filter(name='Steering Committee').exists()
+        return self.user_is_steering
+
+    def has_permission(self, request, view):
+        return request.user and self.is_steering(request.user)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            return request.user.is_staff
+        return self.is_steering(request.user)
+
+
+class ActionPointPermission(SteeringPermission):
+    pass
+
+
+class MeetingPermission(SteeringPermission):
+    pass
