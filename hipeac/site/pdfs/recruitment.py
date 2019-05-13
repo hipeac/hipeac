eecfff496b7b82dba28feb521ab707874072e0a7
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2019-05-13 09:14:01 +0200

    chore: skip errors when generation jobs pdf

--- hipeac/site/pdfs/recruitment.py
+++ hipeac/site/pdfs/recruitment.py
@@ -13,26 +13,31 @@ class JobsPdfMaker:
     def make_pdf(self):
         with Pdf() as pdf:
             for job in self.jobs:
-                location = []
-                if job.location:
-                    location.append(job.location)
-                if job.country:
-                    location.append(job.country.name)
+                try:
+                    location = []
+                    if job.location:
+                        location.append(job.location)
+                    if job.country:
+                        location.append(job.country.name)
 
-                pdf.add_note(f'Find more on our web: hipeac.net/jobs/{job.id}')
-                if job.institution:
-                    if job.institution.image:
-                        pdf.add_image(job.institution.images['th'], 'th')
-                    pdf.add_text(f'<strong>{job.institution.name}</strong>', 'h4')
-                pdf.add_text(', '.join(location), 'h4')
-                pdf.add_spacer()
-                pdf.add_text(job.title, 'h1')
-                pdf.add_text(f'<strong>Deadline</strong>: {date_filter(job.deadline)}', 'ul_li')
-                pdf.add_text(f'<strong>Career levels</strong>: {job.get_metadata_display("career_levels")}', 'ul_li')
-                pdf.add_text(f'<strong>Keywords</strong>: {job.get_metadata_display("topics")}', 'ul_li')
-                pdf.add_spacer()
-                pdf.add_text(job.description, 'p_justify', 'markdown')
-                pdf.add_page_break()
+                    pdf.add_note(f'Find more on our web: hipeac.net/jobs/{job.id}')
+                    if job.institution:
+                        if job.institution.image:
+                            pdf.add_image(job.institution.images['th'], 'th')
+                        pdf.add_text(f'<strong>{job.institution.name}</strong>', 'h4')
+                    pdf.add_text(', '.join(location), 'h4')
+                    pdf.add_spacer()
+                    pdf.add_text(job.title, 'h1')
+                    pdf.add_text(f'<strong>Deadline</strong>: {date_filter(job.deadline)}', 'ul_li')
+                    pdf.add_text(f'<strong>Career levels</strong>: {job.get_metadata_display("career_levels")}', 'ul_li')
+                    pdf.add_text(f'<strong>Keywords</strong>: {job.get_metadata_display("topics")}', 'ul_li')
+                    pdf.add_spacer()
+                    pdf.add_text(job.description, 'p_justify', 'markdown')
+                    pdf.add_page_break()
+
+                except Exception:
+                    pdf.add_text(f'<strong>ERROR FOUND: #{job.id} {job.title}</strong>', 'h4')
+                    pdf.add_page_break()
 
             self._response.write(pdf.get())
 
