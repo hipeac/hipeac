407071994d214b95c7618c3519adb8e705a35b52
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2019-11-25 14:41:36 +0100

    feat: generate Tech Transfer call PDF in admin area

--- hipeac/site/admin/awards.py
+++ hipeac/site/admin/awards.py
@@ -5,6 +5,7 @@ from django.utils.safestring import mark_safe
 
 from hipeac.functions import send_task
 from hipeac.models import PublicationConference, TechTransferCall, TechTransferApplication
+from hipeac.site.pdfs.awards import TechTransferCallPdfMaker
 
 
 @admin.register(PublicationConference)
@@ -27,6 +28,7 @@ class PublicationConferenceAdmin(admin.ModelAdmin):
 
 @admin.register(TechTransferCall)
 class TechTransferCallAdmin(admin.ModelAdmin):
+    actions = ('export_pdf',)
     list_display = ('id', 'start_date', 'end_date', 'applications_link', 'is_active')
 
     def get_queryset(self, request):
@@ -38,6 +40,14 @@ class TechTransferCallAdmin(admin.ModelAdmin):
         url = reverse('admin:hipeac_techtransferapplication_changelist')
         return mark_safe(f'<a href="{url}?call__id__exact={obj.id}">{obj.applications__count}</a>')
     applications_link.short_description = 'Applications'
+
+    def pdf_response(self, calls, filename: str = 'hipeac--calls.pdf', as_attachment: bool = False):
+        maker = TechTransferCallPdfMaker(calls=calls, filename=filename, as_attachment=as_attachment)
+        return maker.response
+
+    def export_pdf(self, request, queryset):
+        return self.pdf_response(queryset)
+    export_pdf.short_description = ('[PDF] Generate printable document for selected calls')
 
 
 @admin.register(TechTransferApplication)
