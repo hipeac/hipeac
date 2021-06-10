b702915315df9331f5b98367ed80827de1d2eeee
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2021-06-11 00:11:44 +0200

    chore: acaces grants

--- hipeac/site/static/js_v2/components/acaces.js
+++ hipeac/site/static/js_v2/components/acaces.js
@@ -1,17 +1,22 @@
 Vue.component('acaces-registrations-table', {
   data: function () {
     return {
+      togglePlaceholder: false,
       dialogVisible: false,
+      mutableGrantedIds: [],
       columns: [
-        {
-          name: 'href'
-        },
         {
           name: 'id',
           field: 'id',
           sortable: true,
           label: 'ID',
           align: 'left',
+        },
+        {
+          name: 'grant',
+          field: 'has_grant',
+          label: 'Grant',
+          align: 'center',
         },
         {
           name: 'user_gender',
@@ -24,7 +29,14 @@ Vue.component('acaces-registrations-table', {
           field: 'user_name',
           sortable: true,
           align: 'left',
-          label: 'Attendee'
+          label: 'Student'
+        },
+        {
+          name: 'country_code',
+          field: 'country_code',
+          sortable: true,
+          align: 'center',
+          label: 'Country'
         },
         {
           name: 'user_affiliation',
@@ -61,6 +73,12 @@ Vue.component('acaces-registrations-table', {
       type: Array,
       required: true
     },
+    grantedIds: {
+      type: Array,
+      default: function () {
+        return [];
+      }
+    },
     uuid: {
       type: String,
       default: null
@@ -68,6 +86,10 @@ Vue.component('acaces-registrations-table', {
     showUser: {
       type: Boolean,
       default: true
+    },
+    grantsPerCountry: {
+      type: [Boolean, Object],
+      default: false
     }
   },
   template: `
@@ -75,14 +97,18 @@ Vue.component('acaces-registrations-table', {
       <q-table flat dense row-key="uuid" :data="registrations" :columns="columns" :pagination.sync="pagination">
         <template v-slot:body="props">
           <q-tr :props="props" class="cursor-pointer" @click="$router.replace({params: {uuid: props.row.uuid}})" :class="{'bg-orange-1': props.row.uuid == uuid}">
-            <q-td key="href" :props="props">
-              <q-icon name="add_circle" color="primary"></q-icon>
-            </q-td>
             <q-td key="id" :props="props"><samp><small>{{ props.row.id }}</small></samp></q-td>
+            <q-td key="grant" :props="props">
+              <q-toggle v-if="props.row.custom_data.grant_requested" size="xs" v-model="mutableGrantedIds" :val="props.row.id" color="green" :disable="disableToggle(props.row)" checked-icon="check" :unchecked-icon="(disableToggle(props.row)) ? null : 'arrow_forward_ios'" />
+              <q-toggle v-else size="xs" v-model="togglePlaceholder" :val="false" color="green" disable class="transparent" unchecked-icon="remove" />
+            </q-td>
             <q-td key="user_gender" :props="props">
-              <q-icon :name="{'male': 'male', 'female': 'female', 'non-binary': 'fiber_manual_record', null: 'fiber_manual_record'}[props.row.user_gender]"></q-icon>
+              <q-icon :name="{'male': 'male', 'female': 'female', 'non_binary': 'toll', null: 'toll'}[props.row.user_gender]"></q-icon>
             </q-td>
             <q-td key="user_name" :props="props">{{ props.row.user_name }}</q-td>
+            <q-td key="country_code" :props="props">
+              <country-flag :code="props.row.country_code"></country-flag>
+            </q-td>
             <q-td key="user_affiliation" :props="props" class="full-width">{{ props.row.user_affiliation }}</q-td>
             <q-td key="date" :props="props"><small>{{ props.row.date.format('LLLL') }}</small></q-td>
             <q-td key="visa_status" :props="props">
@@ -134,7 +160,7 @@ Vue.component('acaces-registrations-table', {
               </q-item>
               <q-separator inset="item"></q-separator>
               <q-item>
-                <q-item-section avatar><q-icon :name="{'male': 'male', 'female': 'female', 'non-binary': 'fiber_manual_record', null: 'fiber_manual_record'}[registration.user_gender]"></q-icon></q-item-section>
+                <q-item-section avatar><q-icon :name="{'male': 'male', 'female': 'female', 'non_binary': 'toll', null: 'toll'}[registration.user_gender]"></q-icon></q-item-section>
                 <q-item-section class="text-capitalize">{{ registration.user_gender }}</q-item-section>
               </q-item>
               <q-separator inset="item"></q-separator>
@@ -166,6 +192,9 @@ Vue.component('acaces-registrations-table', {
               </q-item>
             </q-list>
           </q-card-section>
+          <q-card-section v-show="registration.custom_data.grant_requested" >
+            <q-toggle size="xs" v-model="mutableGrantedIds" :val="registration.id" color="green" />
+          </q-card-section>
           <q-card-actions align="right" class="q-pa-md">
             <q-btn flat label="Close" color="grey" v-close-popup></q-btn>
           </q-card-actions>
@@ -175,8 +204,10 @@ Vue.component('acaces-registrations-table', {
   `,
   computed: {
     registrations: function () {
+      var grantedIds = this.grantedIds;
       return this.data.map(function (obj) {
         obj.date = moment(obj.created_at);
+        obj.country_code = obj.user.profile.institution.country.code || obj.custom_data.profile.country.code;
         obj.user_gender = obj.custom_data.profile.gender || {
           1: 'female',
           2: 'male',
@@ -184,6 +215,7 @@ Vue.component('acaces-registrations-table', {
         }[obj.user.profile.gender_id] || null;
         obj.user_name = obj.user.profile.name;
         obj.user_affiliation = obj.user.profile.institution.name;
+        obj.has_grant = _.has(grantedIds, obj.id);
         return obj;
       });
     },
@@ -202,6 +234,25 @@ Vue.component('acaces-registrations-table', {
           this.dialogVisible = false;
         }
       }
+    }
+  },
+  methods: {
+    disableToggle: function (row) {
+      return this.grantsPerCountry
+          && (this.grantsPerCountry[row.country_code].grants_assigned >= this.grantsPerCountry[row.country_code].grants)
+          && this.mutableGrantedIds.indexOf(row.id) < 0;
+    }
+  },
+  watch: {
+    'mutableGrantedIds': _.debounce(function (val, oldVal) {
+      if (val != this.grantedIds) {
+        this.$root.$emit('granted-ids-updated', val);
+      }
+    }, 250)
+  },
+  created: function () {
+    if (this.grantedIds) {
+      this.mutableGrantedIds = this.grantedIds;
     }
   }
 });
