f1d43b1ffe1f4d8dec0c3b593d620997d2a02698
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2022-07-08 10:30:54 +0200

    chore(admin): allow `Full Name <email@domain.com>' as from in Email models

--- hipeac/models/emails.py
+++ hipeac/models/emails.py
@@ -6,7 +6,11 @@ class Email(models.Model):
     code = models.CharField(max_length=64, unique=True)
     action_name = models.CharField(max_length=128, help_text="This text is shown on the admin area dropdowns.")
     position = models.PositiveSmallIntegerField(default=0)
-    from_email = models.EmailField("from")
+    from_email = models.CharField(
+        "from",
+        help_text="It can be a `Full Name &lt;email@domain.com&gt;` string or just an email address.",
+        max_length=128,
+    )
     cc_emails = ArrayField(models.EmailField(), default=list, blank=True, verbose_name="cc")
     reply_to_email = models.EmailField("reply-to", null=True)
     subject = models.CharField(max_length=255)
