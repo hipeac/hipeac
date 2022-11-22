0d258a2e85c93a13e9e184732df06e6406f25f31
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2022-11-22 11:41:45 +0100

    fix: check member publications

--- hipeac/tasks/dblp.py
+++ hipeac/tasks/dblp.py
@@ -1,7 +1,7 @@
 from celery.decorators import periodic_task, task
 from celery.schedules import crontab
 
-from hipeac.models import PublicationConference, Profile
+from hipeac.models import Member, PublicationConference, Profile
 from hipeac.tools.dblp import process_conference_publications, process_user_publications
 
 
@@ -19,5 +19,5 @@ def extract_publications_for_user(user_id):
 
 @periodic_task(run_every=crontab(day_of_week="mon", hour=12, minute=0))
 def check_member_publications():
-    for profile in Profile.objects.filter(user__member__revocation_date__isnull=True).prefetch_related("links"):
-        process_user_publications(profile)
+    for member in Member.objects.select_related("user__profile").prefetch_related("user__profile__links"):
+        process_user_publications(member.user.profile)
