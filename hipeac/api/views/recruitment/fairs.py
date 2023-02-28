0f9fde3d8eaefdca84c47d9ce727e4f791223240
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-02-28 11:47:39 +0100

    fix: job fairs issues

--- hipeac/api/views/recruitment/fairs.py
+++ hipeac/api/views/recruitment/fairs.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
