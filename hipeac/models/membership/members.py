dbe623d406ab404966ef75c7977070fd414c823b
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2024-07-19 14:05:11 +0200

    +check

--- hipeac/models/membership/members.py
+++ hipeac/models/membership/members.py
@@ -2,11 +2,10 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
-from typing import Optional
 
-from .membership import Membership
 from ..institutions import Institution
 from ..mixins import KeywordsMixin
+from .membership import Membership
 
 
 class Member(KeywordsMixin, models.Model):
@@ -18,16 +17,16 @@ class Member(KeywordsMixin, models.Model):
     date = models.DateField()
     type = models.CharField(max_length=20, choices=Membership.MEMBERSHIP_TYPE_CHOICES)
     gender = models.CharField(max_length=255)
-    advisor = models.ForeignKey(get_user_model(), related_name="_affiliates", on_delete=models.SET_NULL, null=True)
-    institution = models.ForeignKey(Institution, related_name="_i1", on_delete=models.SET_NULL, null=True)
-    second_institution = models.ForeignKey(Institution, related_name="_i2", on_delete=models.SET_NULL, null=True)
+    advisor = models.ForeignKey(get_user_model(), related_name="_affiliates", on_delete=models.DO_NOTHING, null=True)
+    institution = models.ForeignKey(Institution, related_name="_i1", on_delete=models.DO_NOTHING, null=True)
+    second_institution = models.ForeignKey(Institution, related_name="_i2", on_delete=models.DO_NOTHING, null=True)
     is_public = models.BooleanField(default=False)
 
     class Meta:
         db_table = "hipeac_membership_member"
         managed = False
 
-    def get_absolute_url(self) -> Optional[str]:
+    def get_absolute_url(self) -> str | None:
         return reverse("user", args=[self.username]) if self.is_public else None
 
     def affiliates(self):
